a02f5816dc393d103e95c235f42acb394b0ae55a
Igor Carvalho Guimarães <igor2311@gmail.com>
2019-02-19 01:42:59 -0300

    Funcionando membros

---
+++ app/Controllers/Http/ClanController.js
@@ -0,0 +1,25 @@
+'use strict'
+const CRoyale = require('croyale');
+const client = new CRoyale.Client('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQ0MCwiaWRlbiI6IjM2NTU5NTM1NTg1MDYwNDU1NSIsIm1kIjp7fSwidHMiOjE1MzM5MTk4MTAzODh9.pP960WHJcU-Cv3IRa090VkpT7bPfu422GffYM5IN2-k');
+class ClanController {
+    async index({view}){
+        var data = await client.getClan('90LLLPUP')
+        var string = JSON.stringify(data)
+        var convertido = JSON.parse(string)
+        return view.render('inicio',{dados : convertido})
+    }
+    async membros({view}){
+        var data = await client.getClan('90LLLPUP')
+        var string = JSON.stringify(data)
+        var convertido = JSON.parse(string)
+        return view.render('membros',{dados : convertido.members})
+    }
+    async info({view, params}){
+        var data = await client.getPlayer(`${params.tag}`)
+        var string = JSON.stringify(data)
+        var convertido = JSON.parse(string)
+        return `ID digitado ${params.tag}`
+    }
+}
+
+module.exports = ClanController
